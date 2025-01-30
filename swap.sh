59d4dc42efbedf9bbf71b0810bf667ef2f451f9f
Pilz <48645439+pilz0@users.noreply.github.com>
2025-01-30 08:26:21 +0100

    Create swap.sh

---
+++ swap.sh
@@ -0,0 +1,7 @@
+fallocate -l 2G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+swapon --show
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab%
